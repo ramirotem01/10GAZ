0e49301855a7f86a4a5b71acb51ce32c505e9a09
ramirotem01 <132000105+ramirotem01@users.noreply.github.com>
2026-03-10 15:31:07 +0200

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,24 @@
+// firebase-config.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+import { getFirestore, doc, setDoc, getDoc, collection, addDoc, query, where, getDocs, serverTimestamp } 
+    from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
+import { getAuth, signInAnonymously, onAuthStateChanged } 
+    from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBsBic8nsutIzEX_XJp8FV0SG8flUR7GZo",
+  authDomain: "gaz-beb61.firebaseapp.com",
+  projectId: "gaz-beb61",
+  storageBucket: "gaz-beb61.firebasestorage.app",
+  messagingSenderId: "574442694292",
+  appId: "1:574442694292:web:b3b8a6a564dcccc4773681",
+  measurementId: "G-K8LDN4QQSJ"
+};
+
+// אתחול
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const auth = getAuth(app);
+
+// ייצוא המשאבים לשימוש בדפים אחרים
+export { db, auth, doc, setDoc, getDoc, collection, addDoc, query, where, getDocs, signInAnonymously, onAuthStateChanged, serverTimestamp };
