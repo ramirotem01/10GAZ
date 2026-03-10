6c496866c23e739986c7a2c50816069c214f5d15
ramirotem01 <132000105+ramirotem01@users.noreply.github.com>
2026-03-10 18:00:31 +0200

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,6 +1,24 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-import { getFirestore, collection, doc, setDoc, getDoc, getDocs, addDoc, query, where, updateDoc, increment, serverTimestamp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
-import { getAuth, signInAnonymously, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import { 
+    getFirestore, 
+    collection, 
+    doc, 
+    setDoc, 
+    getDoc, 
+    getDocs, 
+    addDoc, 
+    query, 
+    where, 
+    updateDoc, 
+    increment, 
+    serverTimestamp 
+} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+import { 
+    getAuth, 
+    signInAnonymously, 
+    onAuthStateChanged, 
+    signOut 
+} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBsBic8nsutIzEX_XJp8FV0SG8flUR7GZo",
@@ -12,12 +30,27 @@ const firebaseConfig = {
   measurementId: "G-K8LDN4QQSJ"
 };
 
+// אתחול האפליקציה
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const auth = getAuth(app);
 
+// ייצוא הפונקציות לשימוש בשאר דפי האתר
 export { 
-    db, auth, signInAnonymously, onAuthStateChanged, 
-    collection, doc, setDoc, getDoc, getDocs, addDoc, 
-    query, where, updateDoc, increment, serverTimestamp 
+    db, 
+    auth, 
+    signInAnonymously, 
+    onAuthStateChanged, 
+    signOut, // הוספנו את זה כאן
+    collection, 
+    doc, 
+    setDoc, 
+    getDoc, 
+    getDocs, 
+    addDoc, 
+    query, 
+    where, 
+    updateDoc, 
+    increment, 
+    serverTimestamp 
 };
